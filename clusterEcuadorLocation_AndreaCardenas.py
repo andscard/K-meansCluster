95b69b0e9efcc3927af198606f586f3e91385451
andscard <ascs1995@gmail.com>
2024-10-18 19:34:38 -0500

    Creacion de cluster con algoritmo K-means
    Código de generacion de cluster + resultado generado

---
+++ clusterEcuadorLocation_AndreaCardenas.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+file_path = 'data_ecu.txt'
+data = pd.read_csv(file_path, delim_whitespace=True, header=None, names=["Longitude", "Latitude"])
+
+# Data cleaning
+data = data.replace('error', np.nan)
+data = data.replace('999.0', np.nan)
+data = data.dropna()
+
+data['Longitude'] = pd.to_numeric(data['Longitude'], errors='coerce')
+data['Latitude'] = pd.to_numeric(data['Latitude'].str.replace(',', '.'), errors='coerce')
+
+data = data.dropna()
+
+# Aplicarcion de  K-means clustering
+kmeans = KMeans(n_clusters=3, random_state=42)
+data['cluster_label'] = kmeans.fit_predict(data[['Longitude', 'Latitude']])
+
+# Generar archivo con ID de la ubicacion y label del cluster
+output_file = 'clusters.txt'
+with open(output_file, 'w') as f:
+    for loc_id, c_label in enumerate(data['cluster_label']):
+        f.write(f"{loc_id} {c_label}\n")
